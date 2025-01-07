4bb61f4cad137d4603614330aaa5cdb47f30f634
Harsh <hjain_be21@thapar.edu>
2025-01-07 11:18:43 +0530

    changed bat script

--- run_compose.bat
+++ run_compose.bat
@@ -3,13 +3,19 @@ REM Check if Docker is running
 docker info >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     echo Docker is not running. Please start Docker.
+    pause
     exit /b 1
 )
 
-REM Navigate to a specific directory where your docker-compose.yml is located
+REM Pull the latest server and client images
+echo Pulling latest server and client images...
+docker pull hjain2003/lambdaserver:latest
+docker pull hjain2003/lambdaclient:latest
 
+REM Navigate to the root directory where docker-compose.yml is located
+cd /d "%~dp0"
 
 REM Run Docker Compose
-docker-compose up --build
+docker-compose up 
 
 pause
